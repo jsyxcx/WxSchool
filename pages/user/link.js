45a1342d2b5d5e8d7b27fd9faa4b2a2ca1b56fd4
konglm <klm.821222@163.com>
2018-07-30 15:31:53 +0800

    增加关联账号功能

--- pages/user/link.js
+++ pages/user/link.js
@@ -1,67 +1,69 @@
 // pages/user/link.js
-var uuid = require('../../libs/uuid/we-uuidv1')
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    inputPhone: ''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(uuid());
+    if (wx.getStorageInfoSync("phoneNumber") == 'undefined'){
+      
+    }
   },
+  getPhoneNumber: function (e) {
+    console.log({ encryptedData: e.detail.encryptedData, iv: e.detail.iv })
+    var encryptedData = e.detail.encryptedData;
+    var iv = e.detail.iv;
+    var that = this;
+    wx.login({ //登录
+      success: function (res) {
+        var code = res.code;
+        if (code) {
+          console.log('code==' + code)
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
+          wx.request({ //获取手机号
+            url: 'https://jbyj.jiaobaowang.net/WxService/getPhoneNumber',
+            data: {
+              encryptedData: encryptedData, iv: iv, code: code,
+            },
+            method: 'POST', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+            success: function (res) {
+              console.log(res.data);
+              console.log('phoneNumber==' + res.data.RspData.Data.phoneNumber);
+              console.log('purePhoneNumber==' + res.data.RspData.Data.purePhoneNumber);
+              wx.setStorageSync("phoneNumber", res.data.RspData.Data.purePhoneNumber);
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+              if (res.data.RspData.Data.purePhoneNumber == undefined) {
+                wx.showModal({
+                  content: '获取手机号失败，请重新获取！',
+                  showCancel: false,
+                  success: function (res) {
+                    if (res.confirm) {
+                      console.log('用户点击确定')
+                    }
+                  }
+                });
+              } else {
+                that.setData({
+                  inputPhone: res.data.RspData.Data.purePhoneNumber
+                })
+              }
+            },
+            fail: function (res) {
+              console.log(res.data);
+              console.log('is failed')
+            }
+          })
+        } else {
+          console.log('获取用户登录态失败！' + res.errMsg)
+        }
+      }
+    });
   }
 })
--- pages/user/user.js
+++ pages/user/user.js
@@ -56,52 +56,5 @@ Page({
     wx.switchTab({
       url: '../user/user'
     })
-  },
-  getPhoneNumber: function (e) {
-    console.log({ encryptedData: e.detail.encryptedData, iv: e.detail.iv })
-    var encryptedData = e.detail.encryptedData;
-    var iv = e.detail.iv;
-    wx.login({ //登录
-      success: function (res) {
-        var code = res.code;
-        if (code) {
-          console.log('code==' + code)
-
-          wx.request({ //获取手机号
-            url: 'https://jbyj.jiaobaowang.net/WxService/getPhoneNumber',
-            data: {
-              encryptedData: encryptedData, iv: iv, code: code,
-            },
-            method: 'POST', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
-            success: function (res) {
-              console.log(res.data);
-              console.log('phoneNumber==' + res.data.RspData.Data.phoneNumber);
-              console.log('purePhoneNumber==' + res.data.RspData.Data.purePhoneNumber);
-              wx.setStorageSync("phoneNumber", res.data.RspData.Data.purePhoneNumber);
-
-              if (res.data.RspData.Data.phoneNumber == 'undefined') {
-                wx.showModal({
-                  content: '获取手机号失败，请重新点击关联！',
-                  showCancel: false,
-                  success: function (res) {
-                    if (res.confirm) {
-                      console.log('用户点击确定')
-                    }
-                  }
-                });
-              } else {
-                
-              }
-            },
-            fail: function (res) {
-              console.log(res.data);
-              console.log('is failed')
-            }
-          })
-        } else {
-          console.log('获取用户登录态失败！' + res.errMsg)
-        }
-      }
-    });
   }
 })
