0706985852ef60c769cd32dfb6ffe4fc0319d20c
xubangsheng <564675260@qq.com>
2018-08-01 11:07:47 +0800

    作业模块

---
+++ pages/task/taskList.js
@@ -0,0 +1,75 @@
+//taskList.js
+Page(
+  /**
+   * 处理页面中的tapName函数
+   */
+  {tapName:function(event){
+    console.log("event==="+JSON.stringify(event))
+  },
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    console.log("用户下拉了")
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+   wx.showToast({
+     title: '已经到底了',
+   })
+    console.log("已经到底了")
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
