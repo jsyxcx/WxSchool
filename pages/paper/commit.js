be12e049c41daba997f9fe272a492aa43a2c76cc
konglm <klm.821222@163.com>
2018-08-02 17:17:34 +0800

    增加拍照和选择图片

--- pages/paper/commit.js
+++ pages/paper/commit.js
@@ -14,53 +14,43 @@ Page({
   onLoad: function (options) {
   
   },
+  camera: function () {
+    var that = this;
+    wx.showActionSheet({
+      itemList: ['拍照'],
+      itemColor: "#CED63A",
+      success: function (res) {
+        if (!res.cancel) {
+          that.chooseWxImage('camera')
+        }
+      }
+    })
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
   },
+  chooseimage: function () {
+    var that = this;
+    wx.showActionSheet({
+      itemList: ['从相册中选择'],
+      itemColor: "#CED63A",
+      success: function (res) {
+        if (!res.cancel) {
+          that.chooseWxImage('album')
+        }
+      }
+    })
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  chooseWxImage: function (type) {
+    var that = this;
+    wx.chooseImage({
+      sizeType: ['original', 'compressed'],
+      sourceType: [type],
+      success: function (res) {
+        console.log(res);
+        that.setData({
+          tempFilePaths: res.tempFilePaths[0],
+        })
+      }
+    })
   }
 })
