e45b3aab8775e3cc4d851c371d78f8cce8ea7f9d
konglm <klm.821222@163.com>
2018-08-08 15:33:00 +0800

    完成批量修改界面

--- pages/paper/teacher/paper.js
+++ pages/paper/teacher/paper.js
@@ -5,12 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-    select_all: false,
-    middlearr: [],
-    items: [
-      { name: '学生1', checked: false },
-      { name: '学生2', checked: true }
-    ],
+
   },
 
   /**
@@ -18,67 +13,5 @@ Page({
    */
   onLoad: function (options) {
   
-  },
-  // 选择
-  select: function (e) {
-    console.log("选择")
-    var that = this;
-    let arr2 = [];
-    var arr = that.data.items;
-    console.log(arr);
-    var index = e.currentTarget.dataset.id;
-    console.log(index);
-    arr[index].checked = !arr[index].checked;
-    console.log(arr);
-
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i].checked) {
-        arr2.push(arr[i])
-      }
-    };
-    that.setData({
-      items: arr,
-      middlearr: arr2
-    })
-  },
-  // 全选
-  select_all: function () {
-    let that = this;
-    that.setData({
-      select_all: !that.data.select_all
-    })
-    if (that.data.select_all) {
-      let arr = that.data.items;
-      let arr2 = [];
-      for (let i = 0; i < arr.length; i++) {
-        if (arr[i].checked == true) {
-          arr2.push(arr[i]);
-        } else {
-          arr[i].checked = true;
-          arr2.push(arr[i]);
-        }
-      }
-      that.setData({
-        items: arr2,
-        middlearr: arr2
-      })
-    }
-  },
-  // 取消全选
-  select_none: function () {
-    let that = this;
-    that.setData({
-      select_all: !that.data.select_all
-    })
-    let arr = that.data.items;
-    let arr2 = [];
-    for (let i = 0; i < arr.length; i++) {
-      arr[i].checked = false;
-      arr2.push(arr[i]);
-    }
-    that.setData({
-      items: arr2,
-      middlearr: []
-    })
   }
 })
