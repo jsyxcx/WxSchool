b98c9fb4c915a6f60d49a0b0b078f9f68262833d
konglm <klm.821222@163.com>
2018-08-27 15:58:33 +0800

    完成家长端所有静态页面

--- pages/paper/parent/paper.js
+++ pages/paper/parent/paper.js
@@ -5,9 +5,27 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    array: ['语文', '数学', '英语'],
+    index: 0,
+    date1: '2018-08-20',
+    date2: '2018-08-20'
   },
-
+  bindPickerChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index: e.detail.value
+    })
+  },
+  bindDate1Change: function (e) {
+    this.setData({
+      date1: e.detail.value
+    })
+  }, 
+  bindDate2Change: function (e) {
+    this.setData({
+      date2: e.detail.value
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -17,6 +35,11 @@ Page({
   edit: function(){
     wx.navigateTo({
       url: 'commit',
+    })
+  },
+  detail: function () {
+    wx.navigateTo({
+      url: 'paperDetail',
     })
   }
 })
